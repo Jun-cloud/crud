19a8d107ae73e768628a26e3fbe8cf7b1cdc5669
Jun-cloud <kjy8827@gmail.com>
2020-03-17 23:03:55 +0900

    Add comment

--- src/main/java/boot/jpa/crud/domain/BaseTimeEntity.java
+++ src/main/java/boot/jpa/crud/domain/BaseTimeEntity.java
@@ -10,15 +10,25 @@ import javax.persistence.EntityListeners;
 import javax.persistence.MappedSuperclass;
 import java.time.LocalDateTime;
 
+// 생성, 수정 시간을 처리해주는 클래스
+
 @Getter
+// 부모 클래스를 상속받는 자식 클래스에게 매핑 정보만 제공하고 싶을 때 사용
+// 엔티티는 엔티티만 상속받을 수 있기 때문에 클래스를 상속받기 위해 사용
+// 엔티티 종류에 상관없이 공통으로 가지고 있어야 하는 정보가 있을때 많이 사용
 @MappedSuperclass
+// 이 클래스에 Auditing 기능을 포함 시킴
+// Audit는 [감시하다, 감사하다]라는 뜻으로 JPA에서 시간에 대해서 자동으로 값을 넣어주는 기능
 @EntityListeners(AuditingEntityListener.class)
 // abstract : 추상화
 public abstract class BaseTimeEntity {
 
+    // @CreatedDate : 처음 엔티티가 저장될때 날짜를 주입
     @CreatedDate
+    // updatable = false : 처음 생성되면 update 불가능
     @Column(updatable = false)
     private LocalDateTime createdDate;
+    // 엔티티가 수정될때 수정날짜 주입
     @LastModifiedDate
     private LocalDateTime modifiedDate;
 
--- src/main/java/boot/jpa/crud/domain/hero/Hero.java
+++ src/main/java/boot/jpa/crud/domain/hero/Hero.java
@@ -8,28 +8,37 @@ import lombok.NoArgsConstructor;
 
 import javax.persistence.*;
 
-// 기본 생성자 protected로 자동으로 만들어줌
+// @Entity를 사용해 데이터베이스 테이블에 대응하는 객체를 만들어 주는 클래스
+
+// JPA : Java Persistence API
+
+// access = AccessLevel.PROTECTED : 기본 생성자를 protected로 자동으로 만들어줌
+// 프로젝트 코드에서 기본 생성자로 생성하는 것은 막으면서 JPA에서 Entity 클래스를 생성하는 것은 허용하기 위해 추가
 // 클래스 명이 바뀌어도 자동으로 알아서 만들어줌
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
-// Getter 자동 생성; 객체에서는 Setter는 절대! 만들면 안됨; 데이터베이스 엑세스 하는 객체를 따로 만들어 줘야함
+// @Getter : Getter 자동 생성, Getter는 필드의 값을 반환하는 메소드
+// 여기서 Setter는 절대! 만들면 안됨; setter는 데이터베이스 엑세스 하는 객체를 따로 만들어 줘야함
 @Getter
-// database테이블(직접연결되는 객체)로 만들어 주는 것
+// @Entity : database테이블(직접연결되는 객체)로 만들어 주는 것; DB테이블과 매칭될 클래스임을 알려줌
 @Entity
 public class Hero extends BaseTimeEntity {
 
-    // jpa : javax.persistence.Id
+    // @Id : 해당 테이블의 기본키(PK)필드를 직접 할당해주기 위해서 사용
     @Id
+    // @GeneratedValue : 기본키에 원하는 생성 규칙을 지정해준다
     // Oracle 은 GenerationType.SEQUENCE; 나머지는 GenerationType.IDENTITY
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    // null 금지
+    // @Column : 기본적으로 해당 클래스의 필드는 모두 컬럼으로 생성되지만, 컬럼에 추가 변경 사항이 있을 경우 사용
+    // nullable = falsen : ull 금지
     @Column(nullable = false)
     private String name;
     private int age;
-    // 디비에 TEXT로 넣겠다는 말
+    // columnDefinition = "TEXT" : 디비에 TEXT로 넣겠다는 말
     @Column(columnDefinition = "TEXT")
     private String note;
 
+    // @Builder : 해당 클래스의 빌더패턴 클래스를 생성해주고, 생성자 상단에 선언하면 생성자에 포함된 필드만 빌더에 포함한다.
     @Builder
     public Hero(String name, int age, String note) {
         this.name = name;
