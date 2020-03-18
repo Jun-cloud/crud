b4447ab4c467e817a88c16fde6bc537c81efbb2d
Jun-cloud <kjy8827@gmail.com>
2020-03-18 19:19:46 +0900

    Update IndexController

--- src/main/java/boot/jpa/crud/web/IndexController.java
+++ src/main/java/boot/jpa/crud/web/IndexController.java
@@ -1,14 +1,21 @@
 package boot.jpa.crud.web;
 
+import boot.jpa.crud.service.HeroService;
+import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
+@AllArgsConstructor
 public class IndexController {
 
-    @GetMapping("/")
-    public String index() {
+    private HeroService heroService;
 
+    @GetMapping("/")
+    public String index(Model model) {
+        model.addAttribute("title","JUnit project");
+        model.addAttribute("list",heroService.HeroFindAllResponse());
         // 자동으로 잡아줌
         return "index";
     }
