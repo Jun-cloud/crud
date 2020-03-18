25e62e8cb61c9398b23c5543cc5b13fb4894fcce
Jun-cloud <kjy8827@gmail.com>
2020-03-18 19:18:22 +0900

    Update HeroUpdateRequestDto

--- src/main/java/boot/jpa/crud/dto/HeroUpdateRequestDto.java
+++ src/main/java/boot/jpa/crud/dto/HeroUpdateRequestDto.java
@@ -10,12 +10,14 @@ import lombok.Setter;
 @Setter
 public class HeroUpdateRequestDto {
 
+    private Long id;
     private String name;
     private int age;
     private String note;
 
     public Hero toEntity() {
         return Hero.builder()
+                .id(this.id)
                 .name(this.name)
                 .age(this.age)
                 .note(this.note)
