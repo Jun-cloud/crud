4493fb1b7aaec0c0b5c2254b9e258e4d12c89ce3
Jun-cloud <kjy8827@gmail.com>
2020-03-18 19:19:24 +0900

    Update HeroRestController

--- src/main/java/boot/jpa/crud/web/HeroRestController.java
+++ src/main/java/boot/jpa/crud/web/HeroRestController.java
@@ -1,10 +1,37 @@
 package boot.jpa.crud.web;
 
-import org.springframework.web.bind.annotation.RestController;
+import boot.jpa.crud.dto.HeroFindByIdResponseDto;
+import boot.jpa.crud.dto.HeroSaveRequestDto;
+import boot.jpa.crud.dto.HeroUpdateRequestDto;
+import boot.jpa.crud.service.HeroService;
+import lombok.AllArgsConstructor;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
+@AllArgsConstructor
 public class HeroRestController {
 
+    private HeroService heroService;
 
+    @PostMapping("/save")
+    public Long HeroSaveRequest(@RequestBody HeroSaveRequestDto dto) {
+        return heroService.HeroSaveRequest(dto);
+    }
+
+    @PostMapping("/find")
+    public HeroFindByIdResponseDto HeroFindByIdResponse(@RequestBody Long id) {
+        return heroService.HeroFindByIdResponse(id);
+    }
+
+    @PutMapping("/update")
+    public Long HeroUpdateRequest(@RequestBody HeroUpdateRequestDto dto){
+        return heroService.HeroUpdateRequest(dto);
+    }
+
+    @DeleteMapping("/delete")
+    public Long HeroDeleteByIdRequest(@RequestBody Long id) {
+        heroService.HeroDeleteByIdRequest(id);
+        return id;
+    }
 
 }
