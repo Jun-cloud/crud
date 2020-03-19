6ecd6cfb9eff0e719a86fadafd449896a155593a
Jun-cloud <kjy8827@gmail.com>
2020-03-19 18:55:50 +0900

    Add Pagination HeroRepository

--- src/main/java/boot/jpa/crud/domain/hero/HeroRepository.java
+++ src/main/java/boot/jpa/crud/domain/hero/HeroRepository.java
@@ -1,8 +1,17 @@
 package boot.jpa.crud.domain.hero;
 
+import boot.jpa.crud.dto.HeroFindAllResponseDto;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
 
 // Long : Id 데이터 타입(SQL 자동완성을 위해)
 public interface HeroRepository extends JpaRepository<Hero, Long> {
 
+    @Query("select h " +
+            "from Hero h " +
+            "order by h.id desc")
+    Page<HeroFindAllResponseDto> HeroFindAllResponse(Pageable pageable);
 }
